090c1cc0121919eccbe2d43f1d335b984905ffdc
Luke Simmons <luke5083@live.com>
2021-02-05 20:58:08 -0600

    Update Driver Controls
    Update formatting and also some speeds. Changed the buttons for intake and scoring to be the same (R1 = out; L1 = in).

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -8,48 +8,42 @@
 uint_fast8_t speed = 32;
 // int_fast8_t power, turn;
 
-inline void drive()
-{
+inline void drive() {
 	// Change speed for linear movements (Buttons X (increase) and B (decrease))
-	if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_A) && speed < 127)
+	if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_A) && speed < 127)
 		speed++;
-	else if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_Y) && speed > 1)
+	else if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_Y) && speed > 1)
 		speed--;
 
 	// Backwards (Button Down) and move cubes out
-	if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN))
-	{
+	if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
 		motors::driveLeft = -speed;
 		motors::driveRight = -speed;
 	}
 
 	// Forwards (Button Up)
-	if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_UP))
-	{
+	if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
 		motors::driveLeft = speed;
 		motors::driveRight = speed;
 		return;
 	}
 
 	// Turn Left (Button Left)
-	else if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT))
-	{
+	else if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
 		motors::driveLeft = -127;
 		motors::driveRight = 127;
 		return;
 	}
 
 	// Turn Right (Button Right)
-	else if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT))
-	{
+	else if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
 		motors::driveLeft = 127;
 		motors::driveRight = -127;
 		return;
 	}
 
 	// Use analog stick if no drive movement buttons are pressed
-	else
-	{
+	else {
 		//power = controllers::master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 		//turn = controllers::master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
 
@@ -62,71 +56,60 @@ inline void drive()
 	}
 }
 
-inline void intake()
-{
+inline void intake() {
 	// Intake in
-	if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
-	{
-		motors::intakeLeft = -127;
-		motors::intakeRight = -127;
-	}
-	// Intake out
-	else if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
-	{
+	if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
 		motors::intakeLeft = 127;
 		motors::intakeRight = 127;
 	}
+	// Intake out
+	else if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+		motors::intakeLeft = -127;
+		motors::intakeRight = -127;
+	}
 	// Intake hold
-	else
-	{
+	else {
 		motors::intakeLeft = 0;
 		motors::intakeRight = 0;
 	}
 }
 
-inline void scoring()
-{
+inline void scoring() {
+	constexpr uint_fast8_t scoringSpeed = 127;
+
 	// Score down
-	if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-	{
-		motors::scoreLeft = -127;
-		motors::scoreRight = -127;
+	if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+		motors::scoreRight = scoringSpeed;
+		motors::scoreLeft = scoringSpeed;
 	}
 	// Score up
-	else if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-	{
-		motors::scoreLeft = 127;
-		motors::scoreRight = 127;
+	else if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+		motors::scoreLeft = -scoringSpeed;
+		motors::scoreRight = -scoringSpeed;
 	}
 	// Score hold
-	else
-	{
+	else {
 		motors::scoreLeft = 0;
 		motors::scoreRight = 0;
 	}
 }
 
-inline void flap()
-{
+inline void flap() {
 	// Flap in
-	if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_X))
-	{
+	if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_X)) {
 		motors::flap = -127;
 	}
 	// Flap out
-	else if (controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_B))
-	{
-		motors::flap = 127;
+	else if(controllers::master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+		motors::flap = 50;
 	}
 	// Flap hold
-	else
-	{
+	else {
 		motors::flap = 0;
 	}
 }
 
-void opcontrol()
-{
+void opcontrol() {
 	// Make sure we know what's going on
 	pros::lcd::clear_line(0);
 	pros::lcd::print(0, "Driver mode");
@@ -137,8 +120,7 @@ void opcontrol()
 	controllers::master.rumble("-.");
 
 	// Begin control
-	while (true)
-	{
+	while(true) {
 		// Run the stuff
 		drive();
 		intake();
