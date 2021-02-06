09a4a143999bcc4f9eb0e9fe67dbd24905156d61
Luke Simmons <luke5083@live.com>
2021-02-05 21:50:31 -0600

    Add better autonomous stubs
    Can now run from any starting point with correct preprocessor definition.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,7 +2,34 @@
 #include "main.h"
 #include "movement.hpp"
 
+// Autonomous declarations
+void redLeft(), redRight(), blueLeft(), blueRight(), any(), skills();
+
+// Run correct autonomous
 void autonomous() {
-	using namespace movement;
+#ifdef RED_LEFT
+	redLeft();
+#endif
+#ifdef RED_RIGHT
+	redRight();
+#endif
+#ifdef BLUE_LEFT
+	blueLeft();
+#endif
+#ifdef BLUE_RIGHT
+	blueRight();
+#endif
+#ifdef ANY
+	any();
+#endif
+#ifdef SKILLS
+	skills();
+#endif
+}
+
+// Autonomous definitions
+using namespace movement;
+
+void skills() {
 	move(127, 2000, true);
 }
