93fd124d4c269fa6cb201ff9e19f4466e74361c6
Luke Simmons <luke5083@live.com>
2021-02-06 12:55:38 -0600

    Add Skills Autonomous
    Not very good rn

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -30,6 +30,69 @@ void autonomous() {
 // Autonomous definitions
 using namespace movement;
 
+void intake(int speed) {
+	motors::intakeLeft = speed;
+	motors::intakeRight = speed;
+
+	motors::scoreLeft = speed;
+	motors::scoreRight = speed;
+}
+
 void skills() {
-	move(127, 2000, true);
+	// Move forward some
+	move(127, 500);
+
+	// Start intake
+	intake(127);
+	pros::delay(500);
+
+	// Stop robot and intake
+	move(0, 0, 10, true);
+	intake(0);
+	pros::delay(100);
+
+	// Move scorer up
+	motors::flap = -127;
+	pros::delay(500);
+	motors::flap = 127;
+	pros::delay(510);
+	motors::flap = -127;
+	pros::delay(500);
+	motors::flap = 127;
+	pros::delay(510);
+
+	// Turn left
+	move(-127, 127, 500, true);
+	pros::delay(500);
+
+	// Forward
+	move(127, 500, true);
+	pros::delay(500);
+
+	// Turn left
+	move(-127, 127, 500, true);
+	pros::delay(500);
+
+	// Forward
+	move(127, 500, true);
+	pros::delay(500);
+
+	// Score
+	intake(127);
+	pros::delay(2000);
+	intake(0);
+
+	// Forward
+	move(100, 100, true);
+	pros::delay(500);
+
+	// Backward
+	move(-127, 750, true);
+	pros::delay(500);
+
+	// Stop
+	move(0, 0, 10, true);
+	motors::stopAllMotors();
+
+	// Done
 }
